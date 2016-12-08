7634dc45c52eb5dc0ddd61bb641864a78506ef41
Wraith\kriley <qxc0000@gmail.com>
2016-12-08 13:53:22 -0500

    Python parser works. We now have an array of cards for gems, spells and relics which is pasted into the .html file

--- parse.py
+++ parse.py
@@ -14,37 +14,46 @@ def processCards(fileName = "cards.csv"):
             name = row['Name of card']
             cost = int(row['cost'])
             cardType = row['type'].capitalize()
-            text = row['description']
             expansion = row['Product (base, stretch, exp)']
             if cost == 0:
                 continue
             if cardType == "":
                 continue
-            if row['Card Status'] == "":
-                continue
-            card = Card(name, cardType, cost, text, expansion)
+         #   if row['Card Status'] == "":
+         #       continue
+            card = Card(name, cardType, cost, expansion)
             if cardType == 'Spell':
                 cardsSpell.append(card)
-                f.write(str(card)+ " ")
             if cardType == 'Relic':
                 cardsRelic.append(card)
-                f.write(str(card)+ " ")
             if cardType == 'Gem':
                 cardsGem.append(card)
-                f.write(str(card)+ " ")
-        f.close()
+
+    f.write("cardsGem = [")
+    for x in cardsGem:
+        f.write(str(x))
+    f.write("]\n")
+    f.write("cardsSpell = [")
+    for x in cardsSpell:
+        f.write(str(x))
+    f.write("]\n")
+    f.write("cardsRelic = [")
+    for x in cardsRelic:
+        f.write(str(x))
+    f.write("]\n")
+    f.close()
     return
 
 class Card(object):
 
-    def __init__(self, name, cardType, cost, text, expansion):
+    def __init__(self, name, cardType, cost, expansion):
         self.name = name
         self.cardType = cardType
         self.cost = cost
-        self.text = text
         self.expansion = expansion
 
     def __str__(self):
-        return self.name + " " + self.cardType + " " + str(self.cost) + " " + self.expansion
+        return "new Card(\"" + self.name + "\", '" + self.cardType + "', " + str(self.cost) + ", '" + self.expansion + "'),"
+        
 
 processCards()
